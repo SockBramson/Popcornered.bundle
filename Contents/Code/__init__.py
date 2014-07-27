b09c2bbfd50e39fc57781cee9d8c7a02cbb6cd52
SockBramson <sock.bramson+github@gmail.com>
2014-07-27 09:37:28 -0500

    removed extraneous parts

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -100,60 +100,6 @@ def movies(title, url, page=1):
 
     return oc
 
-#################################################################################################################
-@route(PREFIX + '/movies/new', page=int)
-def MLatest(title, url):
-
-
-# The parsing API is followed by .xpath(‘//PARENTELEMENT’): where PARENTELEMENT is the parent element within the XML document for  
-# which you want to return data values to the variables for the videos. For example each video in your RSS feed may be contained 
-# within an Item element tag that has element tags for title, thumbnail, description, etc.
-
-# Open the object container
-    oc = ObjectContainer(title2=title)
-
-# The HTML.ElementFromURL create a tree structure of all the elements, attributes and data in your html documents called a DOM Tree. 
-# This tree structure is necessary to pull data with xpath commands
-    html = HTML.ElementFromURL(url)
-
-# We start a for loop for all the items we want to pull off of the page.    You will need to search the source document and play around with
-# and xpath checker to find the right structure to get you to a point of returning the full list of items you want to pull the data from.
-    for video in html.xpath('//div/div/div/ul/li/ul/li'):
-  # need to check if urls need additions and if image needs any additions to address
-        url = video.xpath('./div/a//@href')[0]
-        # here we are adding the sites domain name with a global variable we set at the start of the channel
-        url = WebsiteURL + url
-        thumb = video.xpath('./div/a/img//@style')[0]
-        # A value that is returned may have extra code around it that needs to be removed. Here we use the replace string method to fix the thumb address
-        thumb = thumb.replace("background-image:url('", '').replace("');", '')
-        title = video.xpath('./div/div/p/a//text()')[0]
-
-# SEE THE LINKS AT THE TOP FOR MORE DETAILED INFO ON XPATH 
-# We are using xpath to get of all the values for each element with the parent element returned by the variable video 
-# the syntax is: video.xpath (‘./CHILDELEMENT’)[FIRSTVALUE].FORMAT or video.xpath (‘./CHILDELEMENT/FORMAT’)[FIRSTVALUE]
-# where CHILDELEMENT is the xpath commands that gets us to the location of the data in the child element ( ex. title, url, date),
-# FIRSTVALUE is the first occurrence of the child element. Usually you want to get all occurences of the child element for 
-# each parent element so we give this a value of [0] (Python starts the count at zero), and FORMAT defines whether we want 
-# to return the data contained in the element as text or as an attribute.  The syntax is are .text or .get(‘ATTRIBUTE’) 
-# or //text() and //@ATTRIBUTE when attached to the child element xpath where ATTRIBUTE is the name of the attribute 
-# you want to return ex. //@href or .get(href) to return the anchor attribute href="www.domainname.com/file.htm"
-
-# this is where the values for each child element we specified are added to the channel as video clip objects the naming scheme for
-# the values passed here are listed in the Framework Documentation for attributes of VideoClipObjects
-
-        oc.add(VideoClipObject(
-            url = url, 
-            title = title, 
-            thumb = Resource.ContentsOfURLWithFallback(thumb, fallback=R(ICON))))
-      
-# This code below is helpful to show when a source is empty
-    if len(oc) < 1:
-        Log ('still no value for objects')
-        return ObjectContainer(header="Empty", message="Unable to display videos for this show right now.")      
-          
-# it will loop through and return the values for all items in the page 
-    return oc
-
 #############################################################################################################################
 
 @route(PREFIX + '/tv')
